d91ae069ecef3f4e6b4e5e8a0592651496c9289b
Anastasija Kudriavceva <anastasija.kudriavceva@gmail.com>
2025-04-22 11:18:37 +0300

    tralala

---
+++ Homework2/Homework2/Homework2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            GameCharacter character = new GameCharacter("Hero", 100, 1);
+            character.TakeDamage(20);
+            character.Heal(10);
+            character.LevelUp();
+            character.TakeDamage(150);
+            character.Heal(-10);
+            character.Heal(200);
+        }
+    }
+}
