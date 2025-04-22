6cc74efe6e5f9248594c54dd28a949966719288f
Anastasija Kudriavceva <anastasija.kudriavceva@gmail.com>
2025-04-22 22:22:07 +0300

    POBEDA!

---
+++ Homework4/Homework4/Homework4/Program.cs
@@ -0,0 +1,21 @@
+﻿using TextProcessingApp;
+
+class Program
+{
+    /// <summary>
+    /// Main method to run the text processing application.
+    /// </summary>
+    static void Main()
+    {
+        Console.WriteLine("Please enter your text. Type exit to finish: ");
+        string inputText = "";
+        string userInput;
+        while ((userInput = Console.ReadLine()) != "exit")
+        {
+            inputText += userInput + " ";
+        }
+        TextProcessor processor = new TextProcessor(inputText.Trim());
+        Console.WriteLine("\nProcessed Sentences:");
+        processor.ProcessText();
+    }
+}
---
+++ Homework4/Homework4/Homework4/TextProcessor.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TextProcessingApp
+{
+    public class TextProcessor
+    {
+        /// <summary>
+        /// The text to be processed.
+        /// </summary>
+        private string _text;
+        public TextProcessor(string text)
+        {
+            _text = text;
+        }
+        /// <summary>
+        /// Processes text: breaks it into sentences, sorts words in each sentence alphabetically.
+        /// </summary>
+        public void ProcessText()
+        {
+            var sentences = Regex.Split(_text, @"(?<=[.!?])\s+");
+            foreach (var sentence in sentences)
+            {
+                if (!ContainsDate(sentence))
+                {
+                    var sortedWords = string.Join(" ", sentence.Split(' ').OrderBy(word => word));
+                    Console.WriteLine(sortedWords);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks if a sentence contains a date
+        /// </summary>
+        private bool ContainsDate(string sentence)
+        {
+            return Regex.IsMatch(sentence, @"\b\d{1,2}[./]\d{1,2}[./]\d{2,4}\b");
+        }
+    }
+}
